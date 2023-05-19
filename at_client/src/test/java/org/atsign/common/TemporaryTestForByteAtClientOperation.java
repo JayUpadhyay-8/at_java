44a203548fc44f025a8194b91df426efd415cfba
Jay Upadhyay <52772566+JayUpadhyay-8@users.noreply.github.com>
2023-05-18 23:33:15 -0400

    Temporary Test driver class for byte operation
    This is a Test Driver class for Project 2.
    What is done: Public Key testing Implemented.

---
+++ at_client/src/test/java/org/atsign/common/TemporaryTestForByteAtClientOperation.java
@@ -0,0 +1,23 @@
+package org.atsign.common;
+
+import org.atsign.client.api.AtClient;
+import org.atsign.common.Keys.PublicKey;
+import org.atsign.common.Keys.SelfKey;
+import org.atsign.common.Keys.SharedKey;
+
+public class TemporaryTest {
+    public static void main(String[] args) throws Exception {
+        AtSign atSign = new AtSign("@16ourfeminist");
+        AtClient atClient = AtClient.withRemoteSecondary("root.atsign.org:64", atSign, true);
+
+        PublicKey pk = new KeyBuilders.PublicKeyBuilder(atSign).key("testPublicKey").build();
+        byte[] value = "testToken".getBytes();
+        String responsePK = atClient.put(pk, value).get();
+        System.out.println("response of public key here -- " + responsePK);
+        byte[] getPublicValue = atClient.getBinary(pk).get();
+        String str = new String(getPublicValue);
+        System.out.println("get public key -- " + str + " get public value -- " + getPublicValue);
+
+    }
+
+}
